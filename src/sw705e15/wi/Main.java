0dd5a15dbcb1ce936e5460f802a6eeb410971a57
marhlder <marhlder@gmail.com>
2015-10-26 15:50:50 +0100

    removed old code

--- src/sw705e15/wi/Main.java
+++ src/sw705e15/wi/Main.java
@@ -16,37 +16,8 @@ import sw705e15.wi.Scissor.UnableToSplitFurtherException;
 
 public class Main
 {
-
-	public static void main(String[] args)
+	public static void main(final String[] args)
 	{
-		// TODO Auto-generated method stub
-
-		// final DoubleMatrix A = DoubleMatrix.rand(4000, 4000);
-		/*
-		 * final DoubleMatrix A = new DoubleMatrix( new double[][]{ {0, 4, 3, 0,
-		 * 0, 0}, {4, 0, 2, 0, 0, 0}, {3, 2, 0, 2, 0, 0}, {0, 0, 2, 0, 2, 3},
-		 * {0, 0, 0, 2, 0, 4}, {0, 0, 0, 3, 4, 0}, });
-		 */
-		/*
-		 * final DoubleMatrix D = new DoubleMatrix( new double[][]{ {7, 0, 0, 0,
-		 * 0, 0}, {0, 6, 0, 0, 0, 0}, {0, 0, 7, 0, 0, 0}, {0, 0, 0, 7, 0, 0},
-		 * {0, 0, 0, 0, 6, 0}, {0, 0, 0, 0, 0, 7}, });
-		 */
-
-		// final DoubleMatrix D = DoubleMatrix.rand(4000, 4000);
-
-		// final DoubleMatrix L = D.sub(A);
-
-		// System.out.println(L.toString().replace(";", "\n"));
-
-		// DoubleMatrix bountifulPenis = Eigen.symmetricEigenvectors(L)[0];
-
-		// System.out.println("DONE");
-		// System.out.println(bountifulPenis.toString().replace(";", "\n"));
-
-		// System.out.println(Eigen.symmetricEigenvalues(L).toString().replace(";",
-		// "\n"));
-
 		FriendshipsData data = null;
 
 		try
